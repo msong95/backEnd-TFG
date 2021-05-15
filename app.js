eb17c063e27c24ca71420eee6830c7e8c3bc172a
david perez <deivitwebmaster@gmail.com>
2021-05-15 17:43:40 +0200

    added mongo atlas db

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config()
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const apiRouter = require('./routes/api');
 
 var app = express();
-require('dotenv').config()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- db.js
+++ db.js
@@ -1,8 +1,23 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/app-brecha", {
+let url = 'mongodb+srv://nodejs:nodejs214365@cluster0.zkdg2.mongodb.net/app-tfg?retryWrites=true&w=majority'
+
+let uri = '';
+
+switch (process.env.ENVIROMENT) {
+    case 'dev':
+        uri = 'mongodb+srv://nodejs:nodejs214365@cluster0.zkdg2.mongodb.net/app-tfg?retryWrites=true&w=majority';
+    case 'local':
+        uri = 'mongodb://localhost:27017/app-brecha';
+}
+
+mongoose
+  .connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
-
+  })
+  .then(response => {
+    if (response) console.log(`base de datos conectada, entorno -> ${process.env.ENVIROMENT} -> url: ${url}`);
+  })
+  .catch(err => err ? console.error('error en la conexion a la base de datos') : null);
 
 module.exports = { mongoose };
