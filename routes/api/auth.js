fbeb429b86bcc2dce18a6e511615cdb1b93ad907
msong <m.song1995@gmail.com>
2021-06-01 02:46:36 +0200

    update ok

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -14,7 +14,7 @@ router.post("/login", async (req, res) => {
   if(response){
     bcrypt.compare(req.body.password, response.password, (err, response) => {
      const token = jwt.sign({ id: response.id, role: 'admin' } , 'olakease');
-      response ? res.json({token}) : res.json({error: '0'})
+      response ? res.json({token, usuario:req.body}) : res.json({error: '0'})
       // response ? res.json({message: 'login correcto'}) : res.json({message: 'login incorrecto'})
     })
   }else{
@@ -28,30 +28,60 @@ router.post("/registro", async (req, res) => {
  
   //buscamos si existe
   const response = await Usuario.findOne({ email: req.body.email }).exec();
-  console.log(response);
-
+  let body=req.body
   //si existe el usuario entonces no se puede registrar
   if(response){
     const token=null;
-    response ? res.json({token}) : res.json({error: '1'})
+    res.jsonp({token})
     // en caso contrario si se puede
   }else{
-    const token=null;
+
       req.body.id = uuid;
       await bcrypt.hash(req.body.password, 10, (err, hass) => {
         req.body.password = hass;
         let newUser = new Usuario(req.body);
+        const token=body;
         newUser.save();
-        response ? res.json({token}) : res.json({error: '0', message: 'error'})
+        res.jsonp({token})
       });
   }
 });
 
 router.post("/modificar", async (req, res) => {
+
+  let body=req.body;
+  await bcrypt.hash(req.body.password, 10, (err, hass) => {
+    req.body.password = hass;
+    Usuario.findOneAndUpdate({email:body.email}, {$set: req.body}, function(error,info){
+      if(error){
+        res.json({
+          resultado: false,
+          msg: 'No se pudo modificar el cliente',
+          err
+      });
+      }else{
+        let newUser = new Usuario(req.body);
+        res.jsonp({newUser})
+      }
+    })
+  });
+
+
+  /*
+  Usuario.findOne({email: req.body.email}).then((usuario) => {
+      usuario = new Usuario(req.body);
+      usuario
+      .save()
+      .then(()=>{
+            res.jsonp({ usuario }); 
+      }) 
+  });
+*/
+  /*
  //criptear clave
   await bcrypt.hash(req.body.password, 10, (err, hass) => {
     req.body.password = hass;
-    console.log("modificamos");
+  
 
     Usuario.updateOne({email:req.body.email},{username: req.body.username, password: req.body.password }, function(
       error,result){
@@ -62,9 +92,9 @@ router.post("/modificar", async (req, res) => {
           }else{
             response ? res.json({result}) : res.json({error: '0', message: 'error'})
           }
-      });
+      }););
   });
+  */
 });
-
 
 module.exports = router;
